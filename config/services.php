2a33e6c10e416a25f72fe81844968598280727c2
ErwannLeRoux <erwann.leroux1@gmail.com>
2023-07-31 16:13:24 +0200

    feat: add opis validator

--- config/services.php
+++ config/services.php
@@ -5,6 +5,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 use KnpLabs\JsonSchema\Collection;
 use KnpLabs\JsonSchema\JsonSchemaInterface;
 use KnpLabs\JsonSchemaBundle\RequestHandler;
+use KnpLabs\JsonSchemaBundle\Validator\OpisValidator;
 use KnpLabs\JsonSchemaBundle\Validator\SwaggestValidator;
 
 return function(ContainerConfigurator $configurator) {
@@ -19,7 +20,7 @@ return function(ContainerConfigurator $configurator) {
     ;
 
     $services->set(SwaggestValidator::class);
-    $services->alias('KnpLabs\JsonSchema\Validator', SwaggestValidator::class);
+    $services->set(OpisValidator::class);
 
     $services->set(Collection::class)
         ->arg('$schemas', tagged_iterator('knp.json_schema'))
